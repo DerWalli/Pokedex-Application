732a9680e43a35cf0e2a7254d9832591396c2e7f
DerWalli <33699620+DerWalli@users.noreply.github.com>
2022-11-22 12:40:00 +0100

    Update script.js as mentioned in CareerFoundry-Task 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-let favoriteFood = ('Chicken');
-alert('Hello World');
-document.write(favoriteFood);
+let pokemonList = [
+    { name: 'Pichu', height: 0.3, type: ['Electric'] },
+    { name: 'Pikachu', height: 0.4, type: ['Electric'] },
+    { name: 'Raichu', height: 0.8, type: ['Electric'] },
+    { name: 'Bulbasaur', height: 0.7, type: ['Grass', 'Poison'] },
+    { name: 'Ivysaur', height: 1, type: ['Grass', 'Poison'] },
+    { name: 'Venusaur', height: 2, type: ['Grass', 'Poison'] },
+    { name: 'Charmander', height: 0.6, type: 'Fire'},
+    { name: 'Charmeleon', height: 1.1, type: 'Fire'},
+    { name: 'Charizard', height: 1.7, type: ['Fire', 'Flying'] },
+    { name: 'Squirtle', height: 0.5, type: 'Water'},
+    { name: 'Wartortle', height: 1, type: 'Water'},
+    { name: 'Blastoise', height: 1.6, type: 'Water'}
+];
